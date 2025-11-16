91c2b3ff093c9d48e9e1b8ae0b2da459053533c2
sapatmohit <sapatmohit18@gmail.com>
2025-11-16 20:44:10 +0530

    build error fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,7 @@
-import { routing } from '@/i18n/routing';
-import { notFound } from 'next/navigation';
-import { ReactNode } from 'react';
-
 export default function RootLayout({
   children,
-  params,
-}: {
-  children: ReactNode;
-  params: { locale: string };
-}) {
-  // Validate that the locale is supported
-  if (params.locale && !routing.locales.includes(params.locale as (typeof routing.locales)[number])) {
-    notFound();
-  }
-
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return children;
 }

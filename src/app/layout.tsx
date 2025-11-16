5d2718d646679da09d13cbd32f920e16510ad639
sapatmohit <sapatmohit18@gmail.com>
2025-11-16 21:35:09 +0530

    fixed SpeedInsight component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,14 @@
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return children;
+  return (
+    <>
+      {children}
+      <SpeedInsights />
+    </>
+  );
 }
